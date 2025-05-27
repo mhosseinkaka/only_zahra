ac74ebe2ac32909bf554cc935c17c3d17fdfad22
mohammadhossein <75811046+mhosseinkaka@users.noreply.github.com>
2025-05-27 16:48:34 +0330

    Add poet selection feature: Update urls

--- poem_display/urls.py
+++ poem_display/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from poem_display.views import get_random_poem
+from poem_display.views import get_random_poem, get_poem_with_param
 
 urlpatterns = [
     path('', get_random_poem, name='random_poem'),
+    path('<int:poem_id>/', get_poem_with_param, name='poem_with_param'),
 ]
